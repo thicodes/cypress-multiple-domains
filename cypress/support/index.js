122f22c179f58d6a3f54bfe87ed574137fa8aadc
thiagoleitedev <thiagoleitebr@gmail.com>
2020-04-21 21:45:08 -0300

    Clean cookies and store every time

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,7 +14,9 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+beforeEach(() => {
+  cy.clearLocalStorage();
+  cy.clearCookies();
+});
